ae8c636f346e0ee948fbe54d0756befa84601239
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-08-31 18:03:38 +0600

    corrected

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -34,8 +34,9 @@ function renderNavAuthArea(elementId) {
       ${!isStaff ? '<a href="orders.html">My Orders</a>' : ''}
       <a href="cart.html">Cart</a>
       <a href="about.html">About Us</a>
-      <button id="logoutBtn" class="link-btn">Logout</button>
       <button class="theme-toggle" onclick="toggleTheme()">🌙</button>
+      <button id="logoutBtn" class="link-btn">Logout</button>
+      
     `;
     document.getElementById('logoutBtn').addEventListener('click', logout);
   } else {
